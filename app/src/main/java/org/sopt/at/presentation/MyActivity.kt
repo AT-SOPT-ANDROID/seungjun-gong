0830de6afb6079fce576688778e6850a72793ae3
seungjunGong <jjw5741@naver.com>
2025-04-11 00:38:33 +0900

    feat: My 뷰 구현 #1
    registerForActivityResult 적용

--- app/src/main/java/org/sopt/at/presentation/MyActivity.kt
+++ app/src/main/java/org/sopt/at/presentation/MyActivity.kt
@@ -1,47 +1,110 @@
 package org.sopt.at.presentation
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.at.R
 import org.sopt.at.ui.theme.ATSOPTANDROIDTheme
+import org.sopt.at.ui.theme.basicColors
 
 class MyActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val userId = intent.getStringExtra("user_id") ?: ""
+        val userPassword = intent.getStringExtra("user_password") ?: ""
+
         setContent {
             ATSOPTANDROIDTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyScreen(userId, userPassword)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyScreen(
+    id: String,
+    password: String,
+) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(horizontal = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        Text(
+            text = "ID: $id",
+            fontSize = 24.sp,
+            color = Color.White,
+            fontWeight = FontWeight.SemiBold
+        )
+        OutlinedButton(
+            onClick = {
+                val intent = Intent().apply {
+                    putExtra("user_id", id)
+                    putExtra("user_password", password)
+                }
+                (context as? Activity)?.setResult(Activity.RESULT_OK, intent)
+                (context as? Activity)?.finish()
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            border = BorderStroke(
+                width = 1.dp,
+                color = basicColors.gray600
+            ),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black,
+            ),
+            shape = RoundedCornerShape(4.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.btn_logout),
+                color = basicColors.gray500,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.SemiBold,
+            )
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MyScreenPreview() {
     ATSOPTANDROIDTheme {
-        Greeting("Android")
+        MyScreen("아이디", "비밀번호")
     }
 }
