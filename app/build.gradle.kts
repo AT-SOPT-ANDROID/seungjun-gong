1d9cf7ae1049989de7579e34f3761bb64697c2ed
seungjunGong <jjw5741@naver.com>
2025-04-15 17:37:54 +0900

    chore: navigation 의존성 추가 #3
    - kotlinx-serialization
    - compose-navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -58,4 +59,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Compose
+    implementation(libs.androidx.compose.navigation)
+    implementation(libs.kotlinx.serialization.json)
 }
--- app/src/main/java/org/sopt/at/presentation/SignUpActivity.kt
+++ app/src/main/java/org/sopt/at/presentation/SignUpActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -52,6 +53,7 @@ class SignUpActivity : ComponentActivity() {
         setContent {
             ATSOPTANDROIDTheme {
                 SignUpScreen()
+                Scaffold {  }
             }
         }
     }
